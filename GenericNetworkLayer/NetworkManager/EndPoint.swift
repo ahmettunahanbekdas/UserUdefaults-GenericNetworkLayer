1e0cfdd3bc5fac6b9b7fd70df2aef5644ca54762
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2024-05-17 11:11:22 +0300

    create folder and secene delegate form

---
+++ GenericNetworkLayer/NetworkManager/EndPoint.swift
@@ -0,0 +1,8 @@
+//
+//  EndPoint.swift
+//  GenericNetworkLayer
+//
+//  Created by Ahmet Tunahan Bekdaş on 17.05.2024.
+//
+
+import Foundation
--- GenericNetworkLayer/View/HomeView.swift
+++ GenericNetworkLayer/View/HomeView.swift
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeView: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //ss
+        self.view.backgroundColor = .blue
     }
-
-
 }
 

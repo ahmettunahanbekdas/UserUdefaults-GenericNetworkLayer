5ba0a98b4e14fec70b0be92d0f35557ede160727
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2024-05-17 10:22:00 +0300

    first commit

---
+++ GenericNetworkLayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GenericNetworkLayer
+//
+//  Created by Ahmet Tunahan Bekdaş on 17.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //ss
+    }
+
+
+}
+

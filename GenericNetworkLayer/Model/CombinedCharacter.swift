89bbd9c7dc84d69f33cd977a848ac49ffb0ebcbe
ahmettunahanbekdas <ahmettunahanbekdas@gmail.com>
2024-06-01 20:31:32 +0300

    createdModel

---
+++ GenericNetworkLayer/Model/CombinedCharacter.swift
@@ -0,0 +1,14 @@
+//
+//  CombinedCharacter.swift
+//  GenericNetworkLayer
+//
+//  Created by Ahmet Tunahan Bekdaş on 24.05.2024.
+//
+
+import Foundation
+
+struct CombinedCharacter: Codable, Equatable {
+    let id: Int?
+    let name: String?
+    let image: String?
+}
